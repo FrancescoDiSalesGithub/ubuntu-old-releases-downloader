d896e9d738efc4384f4f86cbfa9bc929995b79d1
Francesco Di Sales <f.disales@hotmail.com>
2023-05-07 20:13:45 +0200

    Create ubuntu-x86-old.sh

---
+++ ubuntu-x86-old.sh
@@ -0,0 +1,6 @@
+wget https://old-releases.ubuntu.com/releases/warty/warty-release-install-i386.iso
+wget https://old-releases.ubuntu.com/releases/hoary/ubuntu-5.04-install-i386.iso
+wget https://old-releases.ubuntu.com/releases/breezy/ubuntu-5.10-install-i386.iso
+wget https://old-releases.ubuntu.com/releases/dapper/ubuntu-6.06.1-desktop-i386.iso
+wget https://old-releases.ubuntu.com/releases/edgy/ubuntu-6.10-desktop-i386.iso
+wget https://old-releases.ubuntu.com/releases/warty/warty-release-install-i386.iso
